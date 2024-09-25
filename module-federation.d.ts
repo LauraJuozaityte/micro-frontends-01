0ed4caf4fcd56a0c0eb11c6e3bb043ab50e4bd5b
lauraj <laura.juozaityte@accenture.com>
2024-09-25 12:06:45 +0300

    Working, but with warnings.

---
+++ module-federation.d.ts
@@ -0,0 +1,14 @@
+declare module 'mfe2/Nav' {
+    const Nav: React.ComponentType;
+    export default Nav;
+}
+
+declare module 'mfe2/add' {
+  const add: (a: number, b: number) => number;
+  export default add;
+}
+
+declare module 'mfe2/multiplyByTwo' {
+  const multiplyByTwo: (x: number) => number;
+  export default multiplyByTwo;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,73 @@
-/** @type {import('next').NextConfig} */
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+export default {
+  webpack: (config, options) => {
+    const { isServer } = options;
+    
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'mfe1',
+        filename: 'static/chunks/remoteEntry.js',
+
+        remotes: {
+          mfe2: 'mfe2@http://localhost:3001/_next/static/chunks/remoteEntry.js',
+        },
+
+        shared: {
+          // shared modules
+        },
+      })
+    );
+    return config;
+  },
+};
+
+
+
+/*import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config,options ){
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'mfe1',
+        filename: 'static/chunks/remoteEntry.js',
+
+        remotes: {
+          mfe2: 'mfe2@http://localhost:3001/static/chunks/remoteEntry.js',
+        },
+
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: '^18.0.0',
+            eager: true,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: '^18.0.0', 
+            eager: true,
+          },
+        },
+
+        initOptions: {
+          shareStrategy: 'loaded-first',
+        },
+
+        extraOptions: {
+          exposePages: true,
+          enableImageLoaderFix: true,
+          enableUrlLoaderFix: true,
+        },
+      })
+    )
+    return config;
+  },
 };
 
 export default nextConfig;
+*/
